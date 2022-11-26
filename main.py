ddfffbf3eff5743e2a143f9ee31ca1810161f674
Kevin Isom <>
2022-11-26 10:38:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+  This Project is heavily influenced by the Book 'Violent python' by TJ O'Connor       
+  For more Information on said book, please visit the link below 
+    https://www.goodreads.com/book/show/16192263-violent-python
+"""
+from pyFile1 import pyFile1main
+
+def mainFunc():
+  pyFile1main()
+
+mainFunc()
---
+++ pyFile1.py
@@ -0,0 +1,4 @@
+def pyFile1main():
+  result = 'File 1 here'
+  print(result)
+  return result
